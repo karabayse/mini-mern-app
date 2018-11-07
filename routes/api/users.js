6bf229a9c806006a3a6863c8bf5e25c87d5eadb8
Kara Bayse <karabayse@gmail.com>
2018-11-07 12:46:36 -0600

    added GET, POST, DELETE

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,4 +11,21 @@ router.get('/', (req, res) => {
     .then(users => res.json(users));
 });
 
+// POST api/users
+router.post('/', (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+    picture: req.body.picture,
+    description: req.body.description
+  });
+  newUser.save().then(user => res.json(user));
+});
+
+// DELETE api/users/:id
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => user.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}));
+});
+
 module.exports = router;
